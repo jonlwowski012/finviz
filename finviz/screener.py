312d65b59389aeee7c85026a3c76fe617bbb4039
Jonathan Lwowski <jonathan.lwowski@gmail.com>
2024-01-24 14:38:10 -0600

    Update screener.py

--- finviz/screener.py
+++ finviz/screener.py
@@ -414,7 +414,7 @@ class Screener(object):
         """ Private function used to return table headers. """
         headers = []
 
-        header_elements = self._page_content.cssselect('tr[valign="middle"]')[0].xpath("th")
+        header_elements = self._page_content.cssselect('tr[align="center" valign="middle"]')[0].xpath("th")
         
         for header_element in header_elements:
             # Use normalize-space to extract text content while ignoring internal elements
